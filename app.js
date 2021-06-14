63fc6fa38d56293b83858d10915b9874432f13bc
calvin c <calvincrouch365@gmail.com>
2021-06-13 21:59:48 -0500

    upddated INdex.html

---
+++ app.js
@@ -0,0 +1,141 @@
+console.log("app.js loaded");
+
+function DrawBargraph(sampleId) {
+    console.log(`Drawbargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        // console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+        
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks, 
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout); 
+
+
+    });
+}
+
+function DrawBubblechart(sampleId) {
+    d3.json("data/samples.json").then((data) => {
+      // @TODO: Build a Bubble Chart Using the Sample Data
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+      
+      
+        var bubbleLayout = {
+            margin: { t: 0 },
+            hovermode: "closests",
+            xaxis: { title: "OTU ID"}
+        }
+  
+        var bubbleData = [
+        {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+      ]
+
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+  
+    });
+
+}
+
+function ShowMetadata(sampleId) {
+    // read the json file to get data
+    d3.json("data/samples.json").then((data)=> {
+        
+        // get the metadata info for the demographic panel
+        var metadata = data.metadata;
+
+        console.log(metadata)
+
+        // filter meta data info by id
+        var result = metadata.filter(meta => meta.id.toString() === sampleId)[0];
+
+        // select demographic panel to put data
+        var demographicInfo = d3.select("#sample-metadata");
+        
+        // empty the demographic info panel each time before getting new id info
+        demographicInfo.html("");
+
+        // grab the necessary demographic data data for the id and append the info to the panel
+        Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+        });
+    });
+}
+
+
+function optionChanged(newSampleId) {
+    console.log(`User selected ${newSampleId}`);
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    ShowMetadata(newSampleId);
+
+}
+
+
+
+function InitDashboard() {
+    console.log("InitDashboard()");
+
+    // Populate the dropdown
+    var selector = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then(data => {
+        // console.log(data);
+
+        var sampleNames = data.names;
+
+        sampleNames.forEach(sampleId => {
+            selector.append("option")
+                .text(sampleId)
+                .property("value", sampleId);
+        });
+
+        var id = sampleNames[0];
+
+        DrawBargraph(id);
+        DrawBubblechart(id);
+        ShowMetadata(id);
+
+    });
+}
+
+InitDashboard();
